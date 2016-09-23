6fc3b6046863ca522d9a10ca5ade20d34ea3544f
Miriam77 <miriam.macier@interapthq.com>
2016-09-22 23:49:30 -0400

    weather updates

--- Weather/weather.js
+++ Weather/weather.js
@@ -1,25 +1,23 @@
 
 $(function () {
-    $("button#add").click(function (ev) {
-        var zipcode = $("#zipcode").val();
 
-
-        if (zipcode == "") return;
-
-        $("#list").append("<li>"
-            + zipcode + ' <button class="remove">(x)</button>' + "</li>");
-
-        $("").val("");
-
-
-        $("#list").click("button.remove", function (ev) {
-            $(ev.target).parent().fadeOut();
+ 
+//google
+   $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=41501&key=AIzaSyAx1-F-swzH31k5PfXFhEG_TRcfdp69PqQ").done(function(data) {
+       var lat = data.results[0].geometry.location.lat;
+       var lng = data.results[0].geometry.location.lng;
+       console.log(lat,lng);
+   });
+   $.ajax("https://api.darksky.net/forecast/bcaf790b600851512d4bd14890a70a50" + "/37.8145,82.8071", { dataType: "jsonp"}).done(function(data) {
+      console.log(data);
+      $(".getTemp").append(data.currently.temperature, data.summary)
 
 
 
-            $.ajax("http://rest.learncode.academy/api/learncode/friends/57e2c05d1ebec6010006184f") //adding a single person
-.done(function (data){
-    $("#me").append(data.name + " likes " + data.drink); //pulling the data from the http
+     
+});
 
-  });
- });
+
+
+
+
